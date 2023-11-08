b74538a8cece64039007dbeac9c09bbfc9a3a4c2
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2023-11-08 15:58:50 +0800

    Update multiscale_gaussian_normalization_jp2.py

--- multiscale_gaussian_normalization_jp2.py
+++ multiscale_gaussian_normalization_jp2.py
@@ -3,37 +3,53 @@ import matplotlib.pyplot as plt
 import sunpy.map
 import sunkit_image.enhance as enhance
 import cv2
+import os
 
-###########################################################################
+save_or_not = 0
+
+jp2_path = 'E:/Research/Data/HelioViewer/LASCO_C2/20210117/'
+save_path = 'E:/Research/Work/[else]/corona_image_process/'
+
 # Read LASCO C2 raw fits to get colormaps
 raw_file = 'C:/Users/rzhuo/sunpy/data/20210117/lasco_c2/22799618.fts'
 raw_map = sunpy.map.Map(raw_file)
 cmap = raw_map.cmap
 
-# Read LASCO C2 jp2 images processed by Helioviewer.org
-jp2_path = 'E:/Research/Data/HelioViewer/LASCO_C2/20210117/'
-jp2_file = jp2_path + '2021_01_17__00_00_07_509__SOHO_LASCO_C2_white-light.jp2'
-jp2_image = cv2.imread(jp2_file, cv2.IMREAD_UNCHANGED)
+# iterate through .jp2 files
+for root, dirs, files in os.walk(jp2_path):
+    for file in files:
+        if file.endswith('.jp2'):
+            jp2_name = file            
 
-# Save jp2 images as jpg images
-jpg_file = jp2_file[:-4] + '.jpg'
-cv2.imwrite(jpg_file, jp2_image, [cv2.IMWRITE_JPEG_QUALITY, 100])
-jpg_image = cv2.imread(jpg_file, cv2.IMREAD_UNCHANGED)
-jpg_image = jpg_image.astype(float, copy=False)
+            # Read LASCO C2 jp2 images processed by Helioviewer.org
+            jp2_file = jp2_path + jp2_name
+            jp2_image = cv2.imread(jp2_file, cv2.IMREAD_UNCHANGED)
 
-###########################################################################
-# Plot jpg images
-plt.figure()
-plt.imshow(jpg_image,cmap=cmap)
-plt.colorbar()
-plt.title('Helioviewer')
+            # Save jp2 images as jpg images
+            jpg_file = jp2_file[:-4] + '.jpg'
+            cv2.imwrite(jpg_file, jp2_image, [cv2.IMWRITE_JPEG_QUALITY, 100])
+            jpg_image = cv2.imread(jpg_file, cv2.IMREAD_UNCHANGED)
+            jpg_image = jpg_image.astype(float, copy=False)
 
-# Apply Mulitscale Guassian Normalization
-mgn_image = enhance.mgn(jpg_image,sigma=[1.25, 2.5, 5, 10, 20],weights=[0.907,0.976,1,1,1], k=0.7, gamma=1, h=0.9)
+            # Plot jpg images
+            plt.figure(figsize=(12,8))
+            plt.imshow(jpg_image,cmap=cmap)
+            plt.colorbar()
+            plt.title('Helioviewer')
 
-plt.figure()
-plt.imshow(mgn_image,cmap=cmap,vmin=0,vmax=0.8)
-plt.colorbar()
-plt.title('MGN')
+            if save_or_not == 1:
+                plt.savefig(save_path + jp2_name[:21] + 'raw.jpg', format='jpg')
 
-plt.show()
+            # Apply Mulitscale Guassian Normalization
+            mgn_image = enhance.mgn(jpg_image,sigma=[1.25, 2.5, 5, 10, 20],weights=[0.907,0.976,1,1,1], k=0.7, gamma=1, h=0.9)
+
+            plt.figure(figsize=(12,8))
+            plt.imshow(mgn_image,cmap=cmap,vmin=0,vmax=0.8)
+            plt.colorbar()
+            plt.title('MGN')
+
+            if save_or_not == 1:
+                plt.savefig(save_path + jp2_name[:21] + 'mgn.jpg', format='jpg')
+
+            # plt.show()
+            # plt.close()
