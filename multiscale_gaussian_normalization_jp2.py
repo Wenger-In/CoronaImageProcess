331195a7771cb43f48ebd54b650ba758aecafacc
Wenger-In <97772906+Wenger-In@users.noreply.github.com>
2023-11-10 15:58:12 +0800

    Update multiscale_gaussian_normalization_jp2.py

--- multiscale_gaussian_normalization_jp2.py
+++ multiscale_gaussian_normalization_jp2.py
@@ -8,7 +8,6 @@ import os
 save_or_not = 0
 
 jp2_path = 'E:/Research/Data/HelioViewer/LASCO_C2/20210117/'
-# jp2_path = 'C:/Users/rzhuo/sunpy/data/'
 save_path = 'E:/Research/Work/[else]/corona_image_process/'
 
 # Read LASCO C2 raw fits to get colormaps
@@ -16,7 +15,23 @@ raw_file = 'C:/Users/rzhuo/sunpy/data/20210117/lasco_c2/22799618.fts'
 raw_map = sunpy.map.Map(raw_file)
 cmap = raw_map.cmap
 
-# iterate through .jp2 files
+# Select observation slit
+center_x = 512
+center_y = 512
+radius = 400
+dtheta = 0.005
+x_slit = []
+y_slit = []
+
+for theta in np.arange(np.pi/3, 3*np.pi/4, dtheta):
+    x = int(center_x - radius * np.sin(theta))
+    y = int(center_y - radius * np.cos(theta))
+    if not x_slit or not (x == x_slit[-1] and y == y_slit[-1]):
+        x_slit.append(x)
+        y_slit.append(y)
+
+# Iterate through .jp2 files
+slit_image = []
 for root, dirs, files in os.walk(jp2_path):
     for file in files:
         if file.endswith('.jp2'):
@@ -41,18 +56,36 @@ for root, dirs, files in os.walk(jp2_path):
             plt.close()
 
             if save_or_not == 1:
-                plt.savefig(save_path + 'raw/' + jp2_name[:21] + 'raw.jpg', format='jpg')
+                plt.savefig(save_path + 'raw/' + jp2_name[:20] + 'raw.jpg', format='jpg')
 
             # Apply Mulitscale Guassian Normalization
             mgn_image = enhance.mgn(jpg_image,sigma=[1.25, 2.5, 5, 10, 20],weights=[0.907,0.976,1,1,1], k=0.7, gamma=1, h=0.9)
+            
+            # Extract slit observations
+            slit_pixels = [mgn_image[y, x] for x, y in zip(x_slit, y_slit)]
+            slit_pixels = np.array(slit_pixels)
+            slit_pixels = slit_pixels.reshape(-1,1)
+            slit_pixels = np.flipud(slit_pixels)
+            slit_image.append(slit_pixels)
 
+            # Plot MGN images with selected slit
             plt.figure(figsize=(12,8))
             plt.imshow(mgn_image,cmap=cmap,vmin=0,vmax=0.8)
             plt.colorbar()
+            plt.scatter(x_slit,y_slit,c='k')
             plt.title('MGN@' + jp2_name[:21])
 
             if save_or_not == 1:
-                plt.savefig(save_path + 'mgn/' + jp2_name[:21] + 'mgn.jpg', format='jpg')
+                plt.savefig(save_path + 'mgn/' + jp2_name[:20] + 'mgn.jpg', format='jpg')
 
             # plt.show()
             plt.close()
+
+slit_image_array = np.column_stack(slit_image)
+
+plt.figure(figsize=(12,8))
+plt.pcolor(slit_image_array,cmap=cmap,vmin=0,vmax=0.8)
+plt.colorbar()
+plt.title('slit observation')
+
+plt.show()
